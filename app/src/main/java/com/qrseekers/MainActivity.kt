f3134b9339de13b401774ff0a51676de035b7c8c
sajdoann <sajdokova@gmail.com>
2024-10-14 17:14:19 +0100

    authentication, login, singn up, home page and user authentication

--- app/src/main/java/com/qrseekers/MainActivity.kt
+++ app/src/main/java/com/qrseekers/MainActivity.kt
@@ -8,13 +8,8 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
 import com.qrseekers.ui.theme.QRSeekersTheme
-import com.google.firebase.FirebaseApp
 
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/qrseekers/ui/LoginPage.kt
+++ app/src/main/java/com/qrseekers/ui/LoginPage.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -19,10 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.qrseekers.AuthState
